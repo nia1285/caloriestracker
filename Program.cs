f82b89006376a25f84a370a4e037f2fa9c51f97a
nia1285 <55670242+nia1285@users.noreply.github.com>
2019-09-26 22:48:59 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace caloriestracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instance of a tracker == Object
+            var Tracker = new Tracker
+            {
+                Username = "Nia12",
+                dayOfWeek = "Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday",
+                TypeofFood = "abcd",
+                CaloriesAmount = 12345,
+                
+            };
+
+            Tracker.Addition(12345);
+
+            Console.WriteLine($"CT: {Tracker.Username}, $day: {Tracker.dayOfWeek}," +
+                $" $food: {Tracker.TypeofFood},$CA: {Tracker.CaloriesAmount}");
+        }
+    }
+}
---
+++ Tracker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace caloriestracker
+{
+    /// <summary>
+    /// this is a tracker where user can enter the type
+    /// of food and calories intake daily
+    /// </summary>
+    class Tracker
+    {
+        #region Properties
+        /// <summary>
+        /// Name of the user
+        /// </summary>
+        public string Username { get; set; }
+        /// <summary>
+        /// days of the week
+        /// </summary>
+        public string dayOfWeek { get; set; }
+        /// <summary>
+        /// Calories amount user enter 
+        /// </summary>
+        public decimal CaloriesAmount { get; set; }
+        /// <summary>
+        /// User can enter the type of food they eat
+        /// </summary>
+        public string TypeofFood { get; set; }
+        /// <summary>
+        /// Total calories amount user enter for the day
+        /// </summary>
+        public decimal TotalCalories { get; private set; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Add calories amount to the tracker
+        /// </summary>
+        /// <param name="amount">Amount to add</param>
+        public void Addition(decimal amount)
+        {
+            TotalCalories = CaloriesAmount + CaloriesAmount;
+        }
+
+        #endregion
+    }
+}
+
