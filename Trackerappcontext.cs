73813c7d2f78cbb274b7c20ac6eefbcfe231e940
nia1285 <55670242+nia1285@users.noreply.github.com>
2019-11-02 12:14:26 -0700

    Add files via upload

---
+++ Trackerappcontext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace caloriestracker
+{
+    class Trackerappcontext : DbContext
+
+    {
+        public DbSet<Tracker> Trackers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tracker>(entity =>
+           {
+               entity.HasKey(a => a.Username).HasName("PK_Trackers");
+
+               entity.Property(a => a.DayOfWeek).IsRequired();
+
+               entity.Property(a => a.TypeofMeal).IsRequired();
+
+               entity.Property(a => a.CaloriesAmount).IsRequired().HasMaxLength(10);
+
+               entity.ToTable("Trackers");
+
+           });
+        }
+    }
+}
