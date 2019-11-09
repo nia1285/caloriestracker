c8c3218624171d68696229ad7847fb8ea97666a2
nia1285 <55670242+nia1285@users.noreply.github.com>
2019-11-09 00:01:25 -0800

    Add files via upload

---
+++ Transaction.cs
@@ -0,0 +1,19 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace caloriestracker
+{
+    class Transactions
+    {
+        enum TypeofMeal
+        { Breakfast, Lunch, Dinner }
+        public string Username { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public decimal Amount { get; set; }
+
+        public TypeofMeal Meal { get; set; }
+        public decimal TotalCaloriesAmount { get; set; }
+    }
+
+}
