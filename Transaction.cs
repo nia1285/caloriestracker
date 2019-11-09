f01101f37db7ac89a13aedfd2b57ce3b7f7ecb4b
nia1285 <55670242+nia1285@users.noreply.github.com>
2019-11-09 09:00:38 -0800

    Add files via upload

--- Transaction.cs
+++ Transaction.cs
@@ -1,19 +1,10 @@
-﻿    using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace caloriestracker
 {
-    class Transactions
+    class Transaction
     {
-        enum TypeofMeal
-        { Breakfast, Lunch, Dinner }
-        public string Username { get; set; }
-        public DateTime TransactionDate { get; set; }
-        public decimal Amount { get; set; }
-
-        public TypeofMeal Meal { get; set; }
-        public decimal TotalCaloriesAmount { get; set; }
     }
-
 }
